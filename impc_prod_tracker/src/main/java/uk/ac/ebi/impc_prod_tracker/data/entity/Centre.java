f0ebc93c70f95f079e8734d7baedf49eec117000
rwilson-ebi <rwilson@ebi.ac.uk>
2019-02-28 14:41:06 +0000

    Added a repository and entity for the database centre table.

---
+++ impc_prod_tracker/src/main/java/uk/ac/ebi/impc_prod_tracker/data/entity/Centre.java
@@ -0,0 +1,54 @@
+package uk.ac.ebi.impc_prod_tracker.data.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.GenerationType;
+import javax.validation.constraints.NotNull;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor(access=AccessLevel.PRIVATE, force=true)
+@Entity
+public class Centre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String code;
+
+    private String fullName;
+
+    private String contactName;
+
+    private String contactEmail;
+
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    void updatedAt() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
