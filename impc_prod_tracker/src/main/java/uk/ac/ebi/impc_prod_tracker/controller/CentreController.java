006efe7ada469fddc96edd530b5ce64ab91f7cbe
rwilson-ebi <rwilson@ebi.ac.uk>
2019-02-28 16:22:54 +0000

    Added a REST service for centre data.

---
+++ impc_prod_tracker/src/main/java/uk/ac/ebi/impc_prod_tracker/controller/CentreController.java
@@ -0,0 +1,32 @@
+package uk.ac.ebi.impc_prod_tracker.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import uk.ac.ebi.impc_prod_tracker.data.entity.Centre;
+import uk.ac.ebi.impc_prod_tracker.service.CentreService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class CentreController {
+
+    private CentreService centreService;
+
+    public CentreController(CentreService centreService){
+        this.centreService = centreService;
+    }
+
+    @GetMapping(value = {"/centres"})
+    public Iterable<Centre> getCentres()
+    {
+        return centreService.getCentres();
+    }
+
+    @PostMapping(value = {"/centres"}, consumes="application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Centre postCentre(@RequestBody Centre centre){
+        return centreService.createCentre(centre);
+    }
+
+}
---
+++ impc_prod_tracker/src/main/java/uk/ac/ebi/impc_prod_tracker/service/CentreService.java
@@ -0,0 +1,25 @@
+package uk.ac.ebi.impc_prod_tracker.service;
+
+import org.springframework.stereotype.Component;
+import uk.ac.ebi.impc_prod_tracker.data.entity.Centre;
+import uk.ac.ebi.impc_prod_tracker.data.repository.CentreRepository;
+
+import java.util.List;
+
+@Component
+public class CentreService {
+
+    private CentreRepository centreRepository;
+
+    public CentreService(CentreRepository centreRepository){
+        this.centreRepository = centreRepository;
+    }
+
+    public Iterable<Centre> getCentres() {
+        return centreRepository.findAll();
+    }
+
+    public Centre createCentre(Centre centre) {
+        return centreRepository.save(centre);
+    }
+}
