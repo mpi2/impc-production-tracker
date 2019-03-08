afb2276451bbe7126573cb7e8b617cc2f81d735d
Mauricio Martinez <mmartinez@ebi.ac.uk>
2019-03-08 09:13:33 +0000

    jpa-entities-1: Adding jpa entities
    * Add first part of jpa entities

---
+++ impc_prod_tracker/src/main/java/uk/ac/ebi/impc_prod_tracker/data/organization/work_unit/WorkUnit.java
@@ -0,0 +1,63 @@
+/*******************************************************************************
+ * Copyright 2019 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ *******************************************************************************/
+package uk.ac.ebi.impc_prod_tracker.data.organization.work_unit;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import uk.ac.ebi.impc_prod_tracker.data.BaseEntity;
+import uk.ac.ebi.impc_prod_tracker.data.organization.funder.Funder;
+import uk.ac.ebi.impc_prod_tracker.data.organization.person.Person;
+import uk.ac.ebi.impc_prod_tracker.data.organization.work_group.WorkGroup;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import java.util.Set;
+
+@NoArgsConstructor(access= AccessLevel.PRIVATE, force=true)
+@Getter
+@Setter
+@Entity
+public class WorkUnit extends BaseEntity
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String code;
+
+    @ManyToMany
+    @JoinTable(
+        name = "production_unit_person",
+        joinColumns = @JoinColumn(name = "production_unit_id"),
+        inverseJoinColumns = @JoinColumn(name = "person_id"))
+    private Set<Person> people;
+
+    @ManyToMany(mappedBy = "workUnits")
+    private Set<WorkGroup> workGroups;
+
+    @ManyToMany(mappedBy = "workUnits")
+    private Set<Funder> funders;
+}
---
+++ impc_prod_tracker/src/main/java/uk/ac/ebi/impc_prod_tracker/data/project/plan/PlanType.java
@@ -0,0 +1,44 @@
+/*******************************************************************************
+ * Copyright 2019 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ *******************************************************************************/
+package uk.ac.ebi.impc_prod_tracker.data.project.plan;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import uk.ac.ebi.impc_prod_tracker.data.BaseEntity;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@NoArgsConstructor(access= AccessLevel.PRIVATE, force=true)
+@Getter
+@Setter
+@Entity
+public class PlanType extends BaseEntity
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private PlanTypeEnumerator name;
+}
