8fd11ee756307bbfb66b97e12d031f046a2a1d62
rwilson-ebi <rwilson@ebi.ac.uk>
2019-04-23 10:49:58 +0100

    Added tables for loading MGI Alleles and renamed AttempType as AttemptType.

--- impc_prod_tracker/src/main/java/uk/ac/ebi/impc_prod_tracker/data/experiment/attemp_type/AttempTypeRepository.java
+++
@@ -1,7 +0,0 @@
-package uk.ac.ebi.impc_prod_tracker.data.experiment.attemp_type;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface AttempTypeRepository extends CrudRepository<AttempType, Long>
-{
-}
---
+++ impc_prod_tracker/src/main/java/uk/ac/ebi/impc_prod_tracker/data/experiment/attempt_type/AttemptTypeRepository.java
@@ -0,0 +1,7 @@
+package uk.ac.ebi.impc_prod_tracker.data.experiment.attempt_type;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AttemptTypeRepository extends CrudRepository<AttemptType, Long>
+{
+}
