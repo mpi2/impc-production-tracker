84dbe178a92ccc83a54edeb1d61d76d6449141f1
Mauricio Martinez <mmartinez@ebi.ac.uk>
2019-04-25 13:58:11 +0100

    jpa-entities-update
    * Phenotype Attempt.

---
+++ impc_prod_tracker/src/main/java/uk/ac/ebi/impc_prod_tracker/data/biology/phenotype_attempt/PhenotypeAttemptRepository.java
@@ -0,0 +1,7 @@
+package uk.ac.ebi.impc_prod_tracker.data.biology.phenotype_attempt;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PhenotypeAttemptRepository extends CrudRepository<PhenotypeAttempt, Long>
+{
+}
