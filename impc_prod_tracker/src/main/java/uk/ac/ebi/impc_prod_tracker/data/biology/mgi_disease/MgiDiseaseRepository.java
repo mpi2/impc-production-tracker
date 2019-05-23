047e23aa20ace7785268fb5b745128a45fb944aa
rwilson-ebi <rwilson@ebi.ac.uk>
2019-05-23 14:36:52 +0100

    Initial changes to load the MGI disease data.

---
+++ impc_prod_tracker/src/main/java/uk/ac/ebi/impc_prod_tracker/data/biology/mgi_disease/MgiDiseaseRepository.java
@@ -0,0 +1,6 @@
+package uk.ac.ebi.impc_prod_tracker.data.biology.mgi_disease;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MgiDiseaseRepository extends CrudRepository<MgiDisease, Long> {
+}
