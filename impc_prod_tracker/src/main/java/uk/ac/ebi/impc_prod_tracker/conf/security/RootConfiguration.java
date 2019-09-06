ea9e7cb979ea7f5b1862f786dd279083323b3d44
Mauricio Martinez <mauroz77@gmail.com>
2019-09-06 16:52:40 +0100

    Fix failing build.

--- impc_prod_tracker/src/main/java/uk/ac/ebi/impc_prod_tracker/conf/security/RootConfiguration.java
+++ impc_prod_tracker/src/main/java/uk/ac/ebi/impc_prod_tracker/conf/security/RootConfiguration.java
@@ -19,7 +19,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.hateoas.config.EnableEntityLinks;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
