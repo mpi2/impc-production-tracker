73d133099e4d93f589a924dd448bdcdf398c49a2
rwilson-ebi <rwilson@ebi.ac.uk>
2019-09-11 15:26:47 +0100

    Changed the relationship between the WorkGroup and the WorkUnit back to a many-to-many relationship.

--- impc_prod_tracker/src/main/java/uk/ac/ebi/impc_prod_tracker/data/organization/work_group/WorkGroup.java
+++ impc_prod_tracker/src/main/java/uk/ac/ebi/impc_prod_tracker/data/organization/work_group/WorkGroup.java
@@ -52,4 +52,13 @@ public class WorkGroup extends BaseEntity
     @ManyToMany(mappedBy = "workGroups")
     private Set<Funder> funders;
 
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "work_group_work_unit",
+            joinColumns = @JoinColumn(name = "work_group_id"),
+            inverseJoinColumns = @JoinColumn(name = "work_unit_id"))
+    private Set<WorkUnit> workUnits;
+
 }
