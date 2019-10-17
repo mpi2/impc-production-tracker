87e4cfd6e59b42fcc9ccd304e3b66dd47676a2bb
Mauricio Martinez <mauroz77@gmail.com>
2019-10-17 13:00:45 +0100

    Renamed ProjectMapper to ProjectEntityToDtoMapper.
    * Class ProjectMapper was growing a lot with the addition of the logic to convert from DTO to Entity so it was renamed to ProjectEntityToDtoMapper to mamage only that part of the conversion.

--- impc_prod_tracker/src/main/java/uk/ac/ebi/impc_prod_tracker/web/mapping/project/ProjectEntityToDtoMapper.java
+++ impc_prod_tracker/src/main/java/uk/ac/ebi/impc_prod_tracker/web/mapping/project/ProjectEntityToDtoMapper.java
@@ -1,6 +1,20 @@
+/*******************************************************************************
+ * Copyright 2019 EMBL - European Bioinformatics Institute
+ *
+ * Licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the
+ * License.
+ *******************************************************************************/
 package uk.ac.ebi.impc_prod_tracker.web.mapping.project;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.impc_prod_tracker.data.biology.project.Project;
 import uk.ac.ebi.impc_prod_tracker.data.biology.project_intention.ProjectIntention;
@@ -12,36 +26,36 @@ import uk.ac.ebi.impc_prod_tracker.data.biology.project_intention_sequence.SortB
 import uk.ac.ebi.impc_prod_tracker.web.dto.project.ProjectDTO;
 import uk.ac.ebi.impc_prod_tracker.web.dto.species.SpeciesDTO;
 import uk.ac.ebi.impc_prod_tracker.web.dto.status_stamps.StatusStampsDTO;
+import uk.ac.ebi.impc_prod_tracker.web.mapping.EntityMapper;
 import uk.ac.ebi.impc_prod_tracker.web.mapping.gene.ProjectIntentionGeneMapper;
 import uk.ac.ebi.impc_prod_tracker.web.mapping.location.ProjectIntentionLocationMapper;
 import uk.ac.ebi.impc_prod_tracker.web.mapping.sequence.ProjectIntentionSequenceMapper;
 import uk.ac.ebi.impc_prod_tracker.web.mapping.statusStamp.StatusStampMapper;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
-public class ProjectMapper
+public class ProjectEntityToDtoMapper
 {
-    private ModelMapper modelMapper;
+    private EntityMapper entityMapper;
     private StatusStampMapper statusStampMapper;
     private ProjectIntentionGeneMapper projectIntentionGeneMapper;
     private ProjectIntentionLocationMapper projectIntentionLocationMapper;
     private ProjectIntentionSequenceMapper projectIntentionSequenceMapper;
     private static final String MGI_URL = "http://www.mousephenotype.org/data/genes/";
 
-    public ProjectMapper(
-        ModelMapper modelMapper,
+    public ProjectEntityToDtoMapper(
+        EntityMapper entityMapper,
         StatusStampMapper statusStampMapper,
         ProjectIntentionGeneMapper projectIntentionGeneMapper,
         ProjectIntentionLocationMapper projectIntentionLocationMapper,
         ProjectIntentionSequenceMapper projectIntentionSequenceMapper)
     {
-        this.modelMapper = modelMapper;
+        this.entityMapper = entityMapper;
         this.statusStampMapper = statusStampMapper;
         this.projectIntentionGeneMapper = projectIntentionGeneMapper;
         this.projectIntentionLocationMapper = projectIntentionLocationMapper;
@@ -53,7 +67,7 @@ public class ProjectMapper
         ProjectDTO projectDTO = null;
         if (project != null)
         {
-            projectDTO = modelMapper.map(project, ProjectDTO.class);
+            projectDTO = entityMapper.toTarget(project, ProjectDTO.class);
             addStatusStampsDTO(project, projectDTO);
             addProjectIntentionGenes(project, projectDTO);
             addProjectIntentionLocations(project, projectDTO);
