671bab0a6c300daa057093c4f576c4608c4e2032
albags <gomezsegura.alba@gmail.com>
2019-10-29 16:02:19 +0000

    Revoke relation

--- impc_prod_tracker/src/main/java/uk/ac/ebi/impc_prod_tracker/data/organization/project_consortium_institute/ProjectConsortiumInstituteRepository.java
+++
@@ -1,6 +0,0 @@
-package uk.ac.ebi.impc_prod_tracker.data.organization.project_consortium_institute;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProjectConsortiumInstituteRepository extends CrudRepository<ProjectConsortiumInstituteRepository, Long> {
-}
