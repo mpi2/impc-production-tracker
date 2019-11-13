c54666afe46a118f4fbc7e984dd140564ee5448a
Mauricio Martinez <mauroz77@gmail.com>
2019-11-13 13:51:41 +0000

    Updated endpoint to create a project.

--- impc_prod_tracker/src/main/java/uk/ac/ebi/impc_prod_tracker/web/mapping/project/ProjectDtoToEntityMapper.java
+++ impc_prod_tracker/src/main/java/uk/ac/ebi/impc_prod_tracker/web/mapping/project/ProjectDtoToEntityMapper.java
@@ -1,18 +1,18 @@
-/*******************************************************************************
- * Copyright 2019 EMBL - European Bioinformatics Institute
- *
- * Licensed under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific
- * language governing permissions and limitations under the
- * License.
- *******************************************************************************/
+/******************************************************************************
+ Copyright 2019 EMBL - European Bioinformatics Institute
+
+ Licensed under the Apache License, Version 2.0 (the
+ "License"); you may not use this file except in compliance
+ with the License. You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing,
+ software distributed under the License is distributed on an
+ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ either express or implied. See the License for the specific
+ language governing permissions and limitations under the
+ License.
+ */
 package uk.ac.ebi.impc_prod_tracker.web.mapping.project;
 
 import org.springframework.stereotype.Component;
@@ -22,9 +22,9 @@ import uk.ac.ebi.impc_prod_tracker.data.biology.project_consortium.ProjectConsor
 import uk.ac.ebi.impc_prod_tracker.data.biology.project_intention.ProjectIntention;
 import uk.ac.ebi.impc_prod_tracker.web.dto.project.ProjectDTO;
 import uk.ac.ebi.impc_prod_tracker.web.mapping.EntityMapper;
+import uk.ac.ebi.impc_prod_tracker.web.mapping.assignment_status.AssignmentStatusMapper;
 import uk.ac.ebi.impc_prod_tracker.web.mapping.privacy.PrivacyMapper;
 import uk.ac.ebi.impc_prod_tracker.web.mapping.project.consortium.ProjectConsortiumMapper;
-import uk.ac.ebi.impc_prod_tracker.web.mapping.project.consortium.institute.ProjectConsortiumInstituteMapper;
 import uk.ac.ebi.impc_prod_tracker.web.mapping.project.intention.ProjectIntentionMapper;
 import java.util.List;
 import java.util.Set;
@@ -39,27 +39,37 @@ public class ProjectDtoToEntityMapper
     private PrivacyMapper privacyMapper;
     private ProjectConsortiumMapper projectConsortiumMapper;
     private ProjectIntentionMapper projectIntentionMapper;
+    private AssignmentStatusMapper assignmentStatusMapper;
 
     public ProjectDtoToEntityMapper(
         EntityMapper entityMapper,
         PrivacyMapper privacyMapper,
         ProjectConsortiumMapper projectConsortiumMapper,
-        ProjectIntentionMapper projectIntentionMapper)
+        ProjectIntentionMapper projectIntentionMapper,
+        AssignmentStatusMapper assignmentStatusMapper)
     {
         this.entityMapper = entityMapper;
         this.privacyMapper = privacyMapper;
         this.projectConsortiumMapper = projectConsortiumMapper;
         this.projectIntentionMapper = projectIntentionMapper;
+        this.assignmentStatusMapper = assignmentStatusMapper;
     }
 
     public Project toEntity(ProjectDTO projectDTO)
     {
         Project project = entityMapper.toTarget(projectDTO, Project.class);
+        setAssignmentStatus(project, projectDTO);
         setProjectIntention(project, projectDTO);
         setPrivacy(project, projectDTO);
         setConsortia(project, projectDTO);
 
         return project;
+    }
+
+    private void setAssignmentStatus(Project project, ProjectDTO projectDTO)
+    {
+        project.setAssignmentStatus(
+            assignmentStatusMapper.toEntity(projectDTO.getAssignmentStatusName()));
     }
 
     private void setProjectIntention(Project project, ProjectDTO projectDTO)
