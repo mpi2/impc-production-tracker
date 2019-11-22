4481421c69cdb051d6587342f62ed8808e6da201
Mauricio Martinez <mauroz77@gmail.com>
2019-11-22 10:36:57 +0000

    Migration: Change root pom to pom packaging and add core module.

---
+++ impc_prod_tracker/core/src/test/java/org/gentar/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gentar.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
