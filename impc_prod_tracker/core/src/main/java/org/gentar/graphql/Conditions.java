24486b74132b8ec6e7949d97c830e68f261bd6c6
Mauricio Martinez <mauroz77@gmail.com>
2019-12-05 11:24:07 +0000

    Create a GraphQL query builder.

---
+++ impc_prod_tracker/core/src/main/java/org/gentar/graphql/Conditions.java
@@ -0,0 +1,92 @@
+package org.gentar.graphql;
+
+public class Conditions
+{
+    private static final String COLUMN_OPERATOR_STRING_TEMPLATE = "{%s: {%s: \\\"%s\\\"}}";
+    private static final String COLUMN_OPERATOR_NUMBER_TEMPLATE = "{%s: {%s: %d}}";
+
+    // Avoid instantiation.
+    private Conditions()
+    {
+
+    }
+
+    public static String createOrCondition(String conditions)
+    {
+        String orTemplate = "%s: [%s]";
+        return String.format(orTemplate, Operators.OR.getName(), conditions);
+    }
+
+
+    public static String createEqCondition(String column, String value)
+    {
+        return createColumnOperatorStringCondition(column, Operators.EQ.getName(), value);
+    }
+
+    public static String createEqCondition(String column, double value)
+    {
+        return createColumnOperatorNumberCondition(column, Operators.EQ.getName(), value);
+    }
+
+    public static String createNeqCondition(String column, String value)
+    {
+        return createColumnOperatorStringCondition(column, Operators.NEQ.getName(), value);
+    }
+
+    public static String createNeqCondition(String column, double value)
+    {
+        return createColumnOperatorNumberCondition(column, Operators.NEQ.getName(), value);
+    }
+
+    public static String createGtCondition(String column, double value)
+    {
+        return createColumnOperatorNumberCondition(column, Operators.GT.getName(), value);
+    }
+
+    public static String createGteCondition(String column, double value)
+    {
+        return createColumnOperatorNumberCondition(column, Operators.GTE.getName(), value);
+    }
+
+    public static String createLtCondition(String column, double value)
+    {
+        return createColumnOperatorNumberCondition(column, Operators.LT.getName(), value);
+    }
+
+    public static String createLteCondition(String column, double value)
+    {
+        return createColumnOperatorNumberCondition(column, Operators.LTE.getName(), value);
+    }
+
+    public static String createLikeCondition(String column, String value)
+    {
+        return createColumnOperatorStringCondition(column, Operators.LIKE.getName(), value);
+    }
+
+    public static String createNlikeCondition(String column, String value)
+    {
+        return createColumnOperatorStringCondition(column, Operators.NLIKE.getName(), value);
+    }
+
+    public static String createIlikeCondition(String column, String value)
+    {
+        return createColumnOperatorStringCondition(column, Operators.ILIKE.getName(), value);
+    }
+
+    public static String createNilikeCondition(String column, String value)
+    {
+        return createColumnOperatorStringCondition(column, Operators.NILIKE.getName(), value);
+    }
+
+    private static String createColumnOperatorStringCondition(
+        String column, String operator, String value)
+    {
+        return String.format(COLUMN_OPERATOR_STRING_TEMPLATE, column, operator, value);
+    }
+
+    private static String createColumnOperatorNumberCondition(
+        String column, String operator, double value)
+    {
+        return String.format(COLUMN_OPERATOR_NUMBER_TEMPLATE, column, operator, value);
+    }
+}
