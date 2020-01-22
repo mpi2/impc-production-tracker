527d081752442c109e89dea902d30fcba4e62635
rwilson-ebi <rwilson@ebi.ac.uk>
2020-01-22 13:30:35 +0000

    Removed the obsolete OutcomeLocation and PlanOutcome from the data model.

--- impc_prod_tracker/core/src/main/java/org/gentar/biology/outcome_location/OutcomeLocationRepository.java
+++
@@ -1,7 +0,0 @@
-package org.gentar.biology.outcome_location;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface OutcomeLocationRepository extends CrudRepository<OutcomeLocation, Long>
-{
-}
