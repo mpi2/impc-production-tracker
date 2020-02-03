8042fb05fa7bc3237da4db0d471bed8ae0fb3b69
rwilson-ebi <rwilson@ebi.ac.uk>
2020-02-03 12:20:05 +0000

    Removed the outcomeStatusStamp table and added the colonyStatusStamp and SpecimenStatusStamp tables.

--- impc_prod_tracker/core/src/main/java/org/gentar/biology/outcome/status_stamp/OutcomeStatusStampRepository.java
+++
@@ -1,6 +0,0 @@
-package org.gentar.biology.outcome.status_stamp;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface OutcomeStatusStampRepository extends CrudRepository<OutcomeStatusStamp, Long> {
-}
