89e1101f270891d9851e40c3ebb35849e85838c6
Mauricio Martinez <mauroz77@gmail.com>
2020-03-10 11:25:15 +0000

    Complete Mutation JSON#400
    Including more information for mutation. Still missing mutation categorization information.

--- impc_prod_tracker/dto/src/main/java/org/gentar/biology/mutation/MutationDTO.java
+++ impc_prod_tracker/dto/src/main/java/org/gentar/biology/mutation/MutationDTO.java
@@ -1,25 +1,27 @@
 package org.gentar.biology.mutation;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.gentar.biology.gene.ProjectIntentionGeneDTO;
-
 import java.util.List;
 
 @Data
 @RequiredArgsConstructor
 public class MutationDTO
 {
-    @JsonIgnore
     private Long id;
-    private String alleleSymbol;
-    private String mgiAlleleId;
+    private String mgiAlleleAccessionId;
+    private String mgiAlleleSymbolSuperScript;
     private String geneticMutationTypeName;
+    private String molecularMutationTypeName;
+    private Integer genBankFileId;
+    private Integer vcfFileIndex;
+    private Integer bamFileIndex;
+    private String description;
+    private String autoDescription;
+    private Boolean alleleConfirmed;
+    private String alleleSymbolSuperscriptTemplate;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @JsonProperty("mutationGene")
-    private List<ProjectIntentionGeneDTO> projectIntentionGeneDTOS;
+    @JsonProperty("mutationQcResults")
+    private List<MutationQCResultDTO> mutationQCResultDTOs;
 }
---
+++ impc_prod_tracker/dto/src/main/java/org/gentar/biology/mutation/MutationQCResultDTO.java
@@ -0,0 +1,10 @@
+package org.gentar.biology.mutation;
+
+import lombok.Data;
+
+@Data
+public class MutationQCResultDTO
+{
+    private String qcTypeName;
+    private String statusName;
+}
