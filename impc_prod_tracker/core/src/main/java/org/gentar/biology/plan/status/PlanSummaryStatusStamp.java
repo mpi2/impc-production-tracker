f470dbeb3afd269cbcd9733f6979e58b52798533
rwilson-ebi <rwilson@ebi.ac.uk>
2020-03-17 15:44:29 +0000

    Added a plan summary status stamp entity.

---
+++ impc_prod_tracker/core/src/main/java/org/gentar/biology/plan/status/PlanSummaryStatusStamp.java
@@ -0,0 +1,35 @@
+package org.gentar.biology.plan.status;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.gentar.BaseEntity;
+import org.gentar.biology.plan.Plan;
+import org.gentar.biology.status.Status;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor(access= AccessLevel.PUBLIC, force=true)
+@Data
+@Entity
+public class PlanSummaryStatusStamp extends BaseEntity implements Serializable
+{
+    @Id
+    @SequenceGenerator(name = "planSummaryStatusStampSeq", sequenceName = "PLAN_SUMMARY_STATUS_STAMP_SEQ")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "planSummaryStatusStampSeq")
+    private Long id;
+
+    @NotNull
+    @ManyToOne(targetEntity = Plan.class)
+    private Plan plan;
+
+    @NotNull
+    @ManyToOne(targetEntity = Status.class)
+    private Status status;
+
+    @NotNull
+    private LocalDateTime date;
+}
