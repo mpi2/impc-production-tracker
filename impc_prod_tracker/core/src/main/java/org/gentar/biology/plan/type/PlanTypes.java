d1747450adfb31cc6569a613ec3477945e6af7a8
Mauricio Martinez <mauroz77@gmail.com>
2020-04-14 15:30:33 +0100

    Summary status assignation to project and Plan#459
    - Using PlanStateMachineResolver to get the right state machine for a plan when processing an action from a user and when checking the available actions in a plan.

--- impc_prod_tracker/core/src/main/java/org/gentar/biology/plan/type/PlanTypes.java
+++ impc_prod_tracker/core/src/main/java/org/gentar/biology/plan/type/PlanTypes.java
@@ -1,10 +1,12 @@
 package org.gentar.biology.plan.type;
 
+/**
+ * This enum should match the values in plan_type table.
+ */
 public enum PlanTypes
 {
-    PRODUCTION("Production"),
-    PHENOTYPING("Phenotyping"),
-    BREEDING("Breeding");
+    PRODUCTION("production"),
+    PHENOTYPING("phenotyping");
 
     private String typeName;
 
