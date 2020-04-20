0d59783a571e8967f001af6f16da932cbc578407
Mauricio Martinez <mauroz77@gmail.com>
2020-04-20 12:23:25 +0100

    Create processor for changing a crispr plan from "Attempt in Progress" to "Embryos Obtained"#484
    - Add tests.

---
+++ impc_prod_tracker/core/src/test/java/org/gentar/biology/plan/engine/crispr/processors/EmbryosObtainedProcessorTest.java
@@ -0,0 +1,67 @@
+package org.gentar.biology.plan.engine.crispr.processors;
+
+import org.gentar.biology.plan.Plan;
+import org.gentar.biology.plan.attempt.crispr.CrisprAttempt;
+import org.gentar.biology.plan.engine.PlanStateSetter;
+import org.gentar.biology.plan.engine.crispr.CrisprProductionPlanEvent;
+import org.gentar.biology.plan.engine.crispr.CrisprProductionPlanState;
+import org.gentar.test_util.PlanBuilder;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class EmbryosObtainedProcessorTest
+{
+    private EmbryosObtainedProcessor testInstance;
+
+    @Mock
+    private PlanStateSetter planStateSetter;
+
+    @BeforeEach
+    public void setup()
+    {
+        testInstance = new EmbryosObtainedProcessor(planStateSetter);
+    }
+
+    @Test
+    public void testWhenNoEmbryos()
+    {
+        Plan plan = new Plan();
+        CrisprAttempt crisprAttempt = new CrisprAttempt();
+        crisprAttempt.setTotalEmbryosInjected(0);
+        crisprAttempt.setTotalEmbryosSurvived(0);
+        plan.setCrisprAttempt(crisprAttempt);
+
+        testInstance.process(plan);
+
+        verify(planStateSetter, times(0)).setStatusByName(any(Plan.class), any(String.class));
+    }
+
+    @Test
+    public void testWhenEmbryosInjected()
+    {
+        Plan plan = PlanBuilder.getInstance()
+            .withStatus(CrisprProductionPlanState.AttemptInProgress.getInternalName())
+            .build();
+        CrisprAttempt crisprAttempt = new CrisprAttempt();
+        crisprAttempt.setTotalEmbryosInjected(1);
+        crisprAttempt.setTotalEmbryosSurvived(0);
+        plan.setCrisprAttempt(crisprAttempt);
+        plan.setEvent(CrisprProductionPlanEvent.changeToEmbryosObtained);
+
+        testInstance.process(plan);
+
+        verify(
+            planStateSetter,
+            times(1)).setStatusByName(any(Plan.class),
+            eq(CrisprProductionPlanEvent.changeToEmbryosObtained.getEndState().getInternalName()));
+    }
+}
