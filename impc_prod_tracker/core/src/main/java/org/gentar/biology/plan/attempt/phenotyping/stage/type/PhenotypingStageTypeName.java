477b2fcef95d4883945dd0ee8af95da29a9ba63a
Mauricio Martinez <mauroz77@gmail.com>
2020-05-06 17:16:50 +0100

    Unify State Machines for phenotyping stages#555
    - Create enum PhenotypingStageTypeName with the names of the stage types in the database.

---
+++ impc_prod_tracker/core/src/main/java/org/gentar/biology/plan/attempt/phenotyping/stage/type/PhenotypingStageTypeName.java
@@ -0,0 +1,40 @@
+package org.gentar.biology.plan.attempt.phenotyping.stage.type;
+
+import org.gentar.util.LabeledType;
+import java.util.HashMap;
+import java.util.Map;
+
+public enum PhenotypingStageTypeName implements LabeledType
+{
+    EARLY_ADULT("early adult"),
+    LATE_ADULT("late adult"),
+    HAPPLOESSENTIAL("haplo-essential"),
+    EMBRYO("Embryo");
+
+    private static final Map<String, PhenotypingStageTypeName> BY_LABEL = new HashMap<>();
+    static
+    {
+        for (PhenotypingStageTypeName e: values())
+        {
+            BY_LABEL.put(e.label, e);
+        }
+    }
+
+    private final String label;
+
+    PhenotypingStageTypeName(String label)
+    {
+        this.label = label;
+    }
+
+    public static PhenotypingStageTypeName valueOfLabel(String label)
+    {
+        return BY_LABEL.get(label);
+    }
+
+    @Override
+    public String getLabel()
+    {
+        return label;
+    }
+}
