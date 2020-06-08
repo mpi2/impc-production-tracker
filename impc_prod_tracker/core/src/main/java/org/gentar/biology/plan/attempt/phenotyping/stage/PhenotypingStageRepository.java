f9c0298ce20c5238b81ab8e5b78bde200a3f79e7
rwilson-ebi <rwilson@ebi.ac.uk>
2020-06-08 18:39:44 +0100

    Added a method to obtain the maximum pps from the phenotyping stage repository.

--- impc_prod_tracker/core/src/main/java/org/gentar/biology/plan/attempt/phenotyping/stage/PhenotypingStageRepository.java
+++ impc_prod_tracker/core/src/main/java/org/gentar/biology/plan/attempt/phenotyping/stage/PhenotypingStageRepository.java
@@ -1,7 +1,10 @@
 package org.gentar.biology.plan.attempt.phenotyping.stage;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PhenotypingStageRepository extends CrudRepository<PhenotypingStage, Long>
 {
+    @Query("SELECT max(ps.pps) FROM PhenotypingStage ps")
+    String getMaxPps();
 }
