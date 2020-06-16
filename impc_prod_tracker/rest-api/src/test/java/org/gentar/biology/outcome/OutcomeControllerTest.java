7a5c91ee8a53eac9ab2aa1207a09df098ef2a5cd
Mauricio Martinez <mauroz77@gmail.com>
2020-06-16 13:29:15 +0100

    Create integration tests for outcomes endpoints
    - Create test to get all outcomes in a plan and to get a specific colony outcome

--- impc_prod_tracker/rest-api/src/test/java/org/gentar/biology/outcome/OutcomeControllerTest.java
+++ impc_prod_tracker/rest-api/src/test/java/org/gentar/biology/outcome/OutcomeControllerTest.java
@@ -5,6 +5,7 @@ import com.github.springtestdbunit.annotation.DatabaseSetup;
 import com.github.springtestdbunit.annotation.DatabaseTearDown;
 import org.gentar.framework.ControllerTestTemplate;
 import org.gentar.framework.SequenceResetter;
+import org.gentar.framework.TestResourceLoader;
 import org.gentar.framework.db.DBSetupFilesPaths;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,6 +15,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.io.IOException;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -33,15 +36,47 @@ class OutcomeControllerTest extends ControllerTestTemplate
     @Test
     @DatabaseSetup(DBSetupFilesPaths.MULTIPLE_OUTCOMES)
     @DatabaseTearDown(type = DatabaseOperation.DELETE_ALL, value = DBSetupFilesPaths.MULTIPLE_OUTCOMES)
-    void testGetColonyOutcome() throws Exception
+    void testGetAllOutcomesInPlan() throws Exception
     {
         ResultActions resultActions = mvc().perform(MockMvcRequestBuilders
             .get("/api/plans/PIN:0000000001/outcomes")
             .header("Authorization", accessToken))
-            .andExpect(status().isOk());
+            .andExpect(status().isOk())
+            .andDo(document("outcomes/allOutcomes"));
 
         MvcResult result = resultActions.andReturn();
         String contentAsString = result.getResponse().getContentAsString();
         System.out.println(contentAsString);
+        String expectedOutputAsString =
+            loadExpectedResponseFromResource("expectedAllOutcomes.json");
+
+        JSONAssert.assertEquals(expectedOutputAsString, contentAsString, JSONCompareMode.STRICT);
+    }
+
+    @Test
+    @DatabaseSetup(DBSetupFilesPaths.MULTIPLE_OUTCOMES)
+    @DatabaseTearDown(type = DatabaseOperation.DELETE_ALL, value = DBSetupFilesPaths.MULTIPLE_OUTCOMES)
+    void testGetColonyOutcomeInPlan() throws Exception
+    {
+        ResultActions resultActions = mvc().perform(MockMvcRequestBuilders
+            .get("/api/plans/PIN:0000000001/outcomes/TPO:000000000001")
+            .header("Authorization", accessToken))
+            .andExpect(status().isOk())
+            .andDo(document("outcomes/allOutcomes"));
+
+        MvcResult result = resultActions.andReturn();
+        String contentAsString = result.getResponse().getContentAsString();
+        System.out.println(contentAsString);
+        String expectedOutputAsString =
+            loadExpectedResponseFromResource("expectedColonyOutcomeTPO_000000000001.json");
+
+        JSONAssert.assertEquals(expectedOutputAsString, contentAsString, JSONCompareMode.STRICT);
+    }
+
+    private String loadExpectedResponseFromResource(String resourceName)
+        throws IOException
+    {
+        String completeResourcePath = TEST_RESOURCES_FOLDER + resourceName;
+        return TestResourceLoader.loadJsonFromResource(completeResourcePath);
     }
 }
