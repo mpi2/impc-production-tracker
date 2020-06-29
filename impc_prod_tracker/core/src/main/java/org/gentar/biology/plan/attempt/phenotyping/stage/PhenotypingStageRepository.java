abd50d7461fae25ffe3d0d7b86dc1589ca5ccbc2
rwilson-ebi <rwilson@ebi.ac.uk>
2020-06-29 17:42:28 +0100

    Added a method to the PhenotypingStageRepository to return the maximum psn identifier in the database.

--- impc_prod_tracker/core/src/main/java/org/gentar/biology/plan/attempt/phenotyping/stage/PhenotypingStageRepository.java
+++ impc_prod_tracker/core/src/main/java/org/gentar/biology/plan/attempt/phenotyping/stage/PhenotypingStageRepository.java
@@ -1,11 +1,15 @@
 package org.gentar.biology.plan.attempt.phenotyping.stage;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
 public interface PhenotypingStageRepository extends CrudRepository<PhenotypingStage, Long>
 {
+    @Query("SELECT max(ps.psn) FROM PhenotypingStage ps")
+    String getMaxPsn();
+
     PhenotypingStage findByPsn(String psn);
 
     List<PhenotypingStage> findAll();
