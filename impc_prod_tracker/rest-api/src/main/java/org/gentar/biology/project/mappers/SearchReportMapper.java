ea4919b2851c7822221f0b8059910c2c50d76c4b
Mauricio Martinez <mauroz77@gmail.com>
2020-07-15 11:19:39 +0100

    Review /search endpoint
    Fix error when the search term does not bring any results.

--- impc_prod_tracker/rest-api/src/main/java/org/gentar/biology/project/mappers/SearchReportMapper.java
+++ impc_prod_tracker/rest-api/src/main/java/org/gentar/biology/project/mappers/SearchReportMapper.java
@@ -1,6 +1,5 @@
 package org.gentar.biology.project.mappers;
 
-import org.gentar.EntityMapper;
 import org.gentar.Mapper;
 import org.gentar.biology.project.search.SearchReport;
 import org.gentar.common.filters.FilterDTO;
@@ -10,30 +9,44 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Component
 public class SearchReportMapper implements Mapper<SearchReport, SearchReportDTO>
 {
-    private EntityMapper entityMapper;
-    private SearchResultMapper searchResultMapper;
+    private final SearchResultMapper searchResultMapper;
 
-    public SearchReportMapper(EntityMapper entityMapper, SearchResultMapper searchResultMapper)
+    public SearchReportMapper(SearchResultMapper searchResultMapper)
     {
-        this.entityMapper = entityMapper;
         this.searchResultMapper = searchResultMapper;
     }
 
+    @Override
     public SearchReportDTO toDto(SearchReport searchReport)
     {
-        SearchReportDTO searchReportDTO = entityMapper.toTarget(searchReport, SearchReportDTO.class);
-        List<SearchResultDTO> searchResultDTOS =
-            searchReport.getResults().stream()
-                .map(x -> searchResultMapper.toDto(x))
-                .collect(Collectors.toList());
-        searchReportDTO.setResults(searchResultDTOS);
-        searchReportDTO.setFilters(toFilterDtos(searchReport.getFilters()));
+        SearchReportDTO searchReportDTO = new SearchReportDTO();
+        searchReportDTO.setDate(searchReport.getDate());
+        searchReportDTO.setSearchTypeName(searchReport.getSearchType().getName());
+        searchReportDTO.setInputs(searchReport.getInputs());
+        searchReportDTO.setPageMetadata(searchReport.getPageMetadata());
+        setSearchResultsDtos(searchReportDTO, searchReport);
+        setFiltersDto(searchReportDTO, searchReport);
         return searchReportDTO;
+    }
+
+    private void setSearchResultsDtos(SearchReportDTO searchReportDTO, SearchReport searchReport)
+    {
+        List<SearchResultDTO> searchResultDTOS = searchResultMapper.toDtos(searchReport.getResults());
+        searchReportDTO.setResults(searchResultDTOS);
+    }
+
+    private void setFiltersDto(SearchReportDTO searchReportDTO, SearchReport searchReport)
+    {
+        List<FilterDTO> filterDTOS = new ArrayList<>();
+        if (searchReport.getFilters() != null)
+        {
+            searchReport.getFilters().forEach((key, value) -> filterDTOS.add(new FilterDTO(key, value)));
+        }
+        searchReportDTO.setFilters(filterDTOS);
     }
 
     private List<FilterDTO> toFilterDtos(Map<String, List<String>> filters)
