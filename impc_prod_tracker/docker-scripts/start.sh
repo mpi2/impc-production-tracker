58e3d423953aad1ded215f27d075f73a076be8da
rwilson-ebi <rwilson@ebi.ac.uk>
2020-07-17 17:58:40 +0100

    Fixed formatting in the docker start script.

--- impc_prod_tracker/docker-scripts/start.sh
+++ impc_prod_tracker/docker-scripts/start.sh
@@ -2,8 +2,8 @@
 set -e
 [[ -z "${TRACKER_PORT}" ]] && port=8080 || port="${TRACKER_PORT}"
 
-[[ -z "${DOCKER_PRODUCTION}" ]] then
+if [ -z "${DOCKER_PRODUCTION}" ]; then
 	java -Djava.security.egd=file:/dev/./urandom -jar app.jar --server.port="${port}" --spring.profiles.active=docker
 else
 	java -Djava.security.egd=file:/dev/./urandom -jar app.jar --server.port="${port}" --spring.profiles.active=dockerproduction
-fi;
+fi
