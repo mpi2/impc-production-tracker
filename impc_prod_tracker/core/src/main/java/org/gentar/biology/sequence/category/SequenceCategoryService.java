e27e6a42b747b1a9a33343d75fb26722081b3f60
Mauricio Martinez <mauroz77@gmail.com>
2020-08-21 12:30:03 +0100

    Set "outcome sequence" category to sequences when updating/creating a mutation

--- impc_prod_tracker/core/src/main/java/org/gentar/biology/sequence/category/SequenceCategoryService.java
+++ impc_prod_tracker/core/src/main/java/org/gentar/biology/sequence/category/SequenceCategoryService.java
@@ -1,6 +1,9 @@
 package org.gentar.biology.sequence.category;
 
+import org.springframework.cache.annotation.Cacheable;
+
 public interface SequenceCategoryService
 {
     SequenceCategory getSequenceCategoryByName(String name);
+    SequenceCategory getOutcomeSequenceCategory();
 }
--- impc_prod_tracker/core/src/main/java/org/gentar/biology/sequence/category/SequenceCategoryServiceImpl.java
+++ impc_prod_tracker/core/src/main/java/org/gentar/biology/sequence/category/SequenceCategoryServiceImpl.java
@@ -1,19 +1,30 @@
 package org.gentar.biology.sequence.category;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SequenceCategoryServiceImpl implements SequenceCategoryService
 {
-    private SequenceCategoryRepository sequenceCategoryRepository;
+    private final SequenceCategoryRepository sequenceCategoryRepository;
+
+    private final static String OUTCOME_SEQUENCE = "outcome sequence";
 
     public SequenceCategoryServiceImpl (SequenceCategoryRepository sequenceCategoryRepository)
     {
         this.sequenceCategoryRepository = sequenceCategoryRepository;
     }
 
+    @Override
+    @Cacheable("sequenceCategories")
     public SequenceCategory getSequenceCategoryByName(String name)
     {
         return sequenceCategoryRepository.findByNameIgnoreCase(name);
+    }
+
+    @Override
+    public SequenceCategory getOutcomeSequenceCategory()
+    {
+        return getSequenceCategoryByName(OUTCOME_SEQUENCE);
     }
 }
