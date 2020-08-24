5d485c3b3bc4d57c0a7adeebe7c40841782574e5
Mauricio Martinez <mauroz77@gmail.com>
2020-08-24 08:47:47 +0100

    Fix error when creating mutation without specifying the category of the sequence

--- impc_prod_tracker/core/src/main/java/org/gentar/biology/sequence/category/SequenceCategoryService.java
+++ impc_prod_tracker/core/src/main/java/org/gentar/biology/sequence/category/SequenceCategoryService.java
@@ -1,7 +1,5 @@
 package org.gentar.biology.sequence.category;
 
-import org.springframework.cache.annotation.Cacheable;
-
 public interface SequenceCategoryService
 {
     SequenceCategory getSequenceCategoryByName(String name);
--- impc_prod_tracker/rest-api/src/main/java/org/gentar/biology/sequence/SequenceCategoryMapper.java
+++ impc_prod_tracker/rest-api/src/main/java/org/gentar/biology/sequence/SequenceCategoryMapper.java
@@ -3,26 +3,21 @@ package org.gentar.biology.sequence;
 import org.gentar.Mapper;
 import org.gentar.biology.sequence.category.SequenceCategory;
 import org.gentar.biology.sequence.category.SequenceCategoryService;
-import org.gentar.exceptions.UserOperationFailedException;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SequenceCategoryMapper implements Mapper<SequenceCategory, String>
 {
-    private SequenceCategoryService sequenceCategoryService;
+    private final SequenceCategoryService sequenceCategoryService;
 
-    private static final String SEQUENCE_CATEGORY_NOT_FOUND_ERROR = "Sequence category '%s' does not exist.";
-
-    public SequenceCategoryMapper(SequenceCategoryService sequenceCategoryService) { this.sequenceCategoryService = sequenceCategoryService; }
+    public SequenceCategoryMapper(SequenceCategoryService sequenceCategoryService)
+    {
+        this.sequenceCategoryService = sequenceCategoryService;
+    }
 
     public SequenceCategory toEntity(String categoryName)
     {
-        SequenceCategory sequenceCategory = sequenceCategoryService.getSequenceCategoryByName(categoryName);
-        if (sequenceCategory == null)
-        {
-            throw new UserOperationFailedException(String.format(SEQUENCE_CATEGORY_NOT_FOUND_ERROR, categoryName));
-        }
-        return sequenceCategory;
+        return sequenceCategoryService.getSequenceCategoryByName(categoryName);
     }
 
     public String toDto(SequenceCategory entity)
