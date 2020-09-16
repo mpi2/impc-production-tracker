1b3253ae1769a92ec638540e9b8c8d834033c16f
Mauricio Martinez <mauroz77@gmail.com>
2020-09-16 11:37:59 +0100

    Add data to configuration endpoint
    - Add work groups by work units
    - Add attempt types by plan types
    - Add funders by work groups

--- impc_prod_tracker/core/src/main/java/org/gentar/biology/plan/attempt/AttemptTypeServiceImpl.java
+++ impc_prod_tracker/core/src/main/java/org/gentar/biology/plan/attempt/AttemptTypeServiceImpl.java
@@ -1,21 +1,43 @@
 package org.gentar.biology.plan.attempt;
 
+import org.gentar.biology.plan.type.PlanTypeName;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 public class AttemptTypeServiceImpl implements AttemptTypeService
 {
-    private AttemptTypeRepository attemptTypeRepository;
+    private final AttemptTypeRepository attemptTypeRepository;
 
     public AttemptTypeServiceImpl (AttemptTypeRepository attemptTypeRepository)
     {
         this.attemptTypeRepository = attemptTypeRepository;
     }
 
+    @Override
     @Cacheable("attemptTypes")
     public AttemptType getAttemptTypeByName(String attemptTypeName)
     {
         return attemptTypeRepository.findByNameIgnoreCase(attemptTypeName);
+    }
+
+    @Override
+    public List<AttemptTypesName> getAttemptTypesByPlanTypeName(PlanTypeName planTypeName)
+    {
+        List<AttemptTypesName> attemptTypesNames = new ArrayList<>();
+        if (PlanTypeName.PRODUCTION.equals(planTypeName))
+        {
+            attemptTypesNames.add(AttemptTypesName.CRISPR);
+            attemptTypesNames.add(AttemptTypesName.HAPLOESSENTIAL_CRISPR);
+        }
+        else if (PlanTypeName.PHENOTYPING.equals(planTypeName))
+        {
+            attemptTypesNames.add(AttemptTypesName.ADULT_PHENOTYPING);
+            attemptTypesNames.add(AttemptTypesName.HAPLOESSENTIAL_PHENOTYPING);
+        }
+        return attemptTypesNames;
     }
 }
