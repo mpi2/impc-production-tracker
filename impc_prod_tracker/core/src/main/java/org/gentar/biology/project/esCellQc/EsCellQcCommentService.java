443ca763fc833f1d40a9a90fb8fd44a7b731fdf8
rwilson-ebi <rwilson@ebi.ac.uk>
2021-07-02 10:36:41 +0100

    Initial work to add the projectEsCellQc data to the project. Tests still need attention.

---
+++ impc_prod_tracker/core/src/main/java/org/gentar/biology/project/esCellQc/EsCellQcCommentService.java
@@ -0,0 +1,6 @@
+package org.gentar.biology.project.esCellQc;
+
+
+public interface EsCellQcCommentService {
+    EsCellQcComment getEsCellQcCommentByName(String name);
+}
---
+++ impc_prod_tracker/dto/src/main/java/org/gentar/biology/project/project_es_cell_qc/EsCellQcCommentDTO.java
@@ -0,0 +1,12 @@
+package org.gentar.biology.project.project_es_cell_qc;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class EsCellQcCommentDTO {
+
+    private String name;
+
+}
