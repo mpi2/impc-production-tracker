e74d3a81cb2b9c4568b4bd256ba1239a54fd7dce
ogunes <ogunes@ebi.ac.uk>
2022-06-17 13:36:47 +0100

    empty projects filtered

--- impc_prod_tracker/core/src/main/java/org/gentar/biology/project/ProjectServiceImpl.java
+++ impc_prod_tracker/core/src/main/java/org/gentar/biology/project/ProjectServiceImpl.java
@@ -13,6 +13,7 @@
  language governing permissions and limitations under the
  License.
  */
+
 package org.gentar.biology.project;
 
 import org.gentar.audit.history.HistoryService;
@@ -44,8 +45,7 @@ import java.util.Map;
 import java.util.Set;
 
 @Component
-public class ProjectServiceImpl implements ProjectService
-{
+public class ProjectServiceImpl implements ProjectService {
     private final ProjectRepository projectRepository;
     private final HistoryService<Project> historyService;
     private final ProjectCreator projectCreator;
@@ -63,8 +63,7 @@ public class ProjectServiceImpl implements ProjectService
         ProjectQueryHelper projectQueryHelper,
         ProjectUpdater projectUpdater,
         ProjectValidator projectValidator,
-        PlanValidator planValidator)
-    {
+        PlanValidator planValidator) {
         this.projectRepository = projectRepository;
         this.historyService = historyService;
         this.projectCreator = projectCreator;
@@ -76,11 +75,9 @@ public class ProjectServiceImpl implements ProjectService
     }
 
     @Override
-    public Project getNotNullProjectByTpn(String tpn)
-    {
+    public Project getNotNullProjectByTpn(String tpn) {
         Project project = projectRepository.findByTpn(tpn);
-        if (project == null)
-        {
+        if (project == null) {
             throw new NotFoundException("Project " + tpn + " does not exist.");
         }
         projectValidator.validateReadPermissions(project);
@@ -89,14 +86,12 @@ public class ProjectServiceImpl implements ProjectService
     }
 
     @Override
-    public Project getProjectByPinWithoutCheckPermissions(String tpn)
-    {
+    public Project getProjectByPinWithoutCheckPermissions(String tpn) {
         return projectRepository.findByTpn(tpn);
     }
 
     @Override
-    public List<Project> getProjects(ProjectFilter projectFilter)
-    {
+    public List<Project> getProjects(ProjectFilter projectFilter) {
         Specification<Project> specifications = buildSpecificationsWithCriteria(projectFilter);
         List<Project> projects = projectRepository.findAll(specifications);
         addOrthologs(projects);
@@ -104,24 +99,21 @@ public class ProjectServiceImpl implements ProjectService
     }
 
     @Override
-    public List<Project> getProjectsWithoutOrthologs(ProjectFilter projectFilter)
-    {
+    public List<Project> getProjectsWithoutOrthologs(ProjectFilter projectFilter) {
         Specification<Project> specifications = buildSpecificationsWithCriteria(projectFilter);
         List<Project> projects = projectRepository.findAll(specifications);
         return projectValidator.getCheckedCollection(projects);
     }
 
     @Override
-    public Page<Project> getProjects(ProjectFilter projectFilter, Pageable pageable)
-    {
+    public Page<Project> getProjects(ProjectFilter projectFilter, Pageable pageable) {
         Specification<Project> specifications = buildSpecificationsWithCriteria(projectFilter);
         Page<Project> projectsPage = projectRepository.findAll(specifications, pageable);
         addOrthologs(projectsPage.getContent());
         return projectsPage.map(projectValidator::getAccessChecked);
     }
 
-    private void addOrthologs(List<Project> projects)
-    {
+    private void addOrthologs(List<Project> projects) {
         List<String> accIds = new ArrayList<>();
         projects.forEach(x -> accIds.addAll(projectQueryHelper.getAccIdsByProject(x)));
         Map<String, List<Ortholog>> orthologs = orthologService.getOrthologsByAccIds(accIds);
@@ -134,13 +126,11 @@ public class ProjectServiceImpl implements ProjectService
         });
     }
 
-    private List<Ortholog> calculateBestOrthologs(List<Ortholog> orthologsByAccId)
-    {
+    private List<Ortholog> calculateBestOrthologs(List<Ortholog> orthologsByAccId) {
         return orthologService.calculateBestOrthologs(orthologsByAccId);
     }
 
-    private List<ProjectIntentionGene> getIntentionGenesByProjects(List<Project> projects)
-    {
+    private List<ProjectIntentionGene> getIntentionGenesByProjects(List<Project> projects) {
         List<ProjectIntentionGene> projectIntentionGenes = new ArrayList<>();
         projects.forEach(x -> {
             projectIntentionGenes.addAll(projectQueryHelper.getIntentionGenesByProject(x));
@@ -148,61 +138,57 @@ public class ProjectServiceImpl implements ProjectService
         return projectIntentionGenes;
     }
 
-    private Specification<Project> buildSpecificationsWithCriteria(ProjectFilter projectFilter)
-    {
+    private Specification<Project> buildSpecificationsWithCriteria(ProjectFilter projectFilter) {
         Specification<Project> specifications =
             Specification.where(
                 ProjectSpecs.withTpns(projectFilter.getTpns())
-                .and(ProjectSpecs.withMarkerSymbols(projectFilter.getMarkerSymbols()))
-                .and(ProjectSpecs.withMarkerSymbolOrAccId(projectFilter.getGenes()))
-                .and(ProjectSpecs.withIntentions(projectFilter.getIntentions()))
-                .and(ProjectSpecs.withPlansInWorkUnitsNames(projectFilter.getWorkUnitNames()))
-                .and(ProjectSpecs.withPlansInWorkGroupNames(projectFilter.getWorGroupNames()))
-                .and(ProjectSpecs.withConsortia(projectFilter.getConsortiaNames()))
-                .and(ProjectSpecs.withAssignments(projectFilter.getAssginmentNames()))
-                .and(ProjectSpecs.withSummaryStatuses(projectFilter.getSummaryStatusNames()))
-                .and(ProjectSpecs.withPrivacies(projectFilter.getPrivaciesNames())))
+                    .and(ProjectSpecs.withMarkerSymbols(projectFilter.getMarkerSymbols()))
+                    .and(ProjectSpecs.withMarkerSymbolOrAccId(projectFilter.getGenes()))
+                    .and(ProjectSpecs.withIntentions(projectFilter.getIntentions()))
+                    .and(ProjectSpecs.withPlansInWorkUnitsNames(projectFilter.getWorkUnitNames()))
+                    .and(ProjectSpecs.withPlansInWorkGroupNames(projectFilter.getWorGroupNames()))
+                    .and(ProjectSpecs.withConsortia(projectFilter.getConsortiaNames()))
+                    .and(ProjectSpecs.withAssignments(projectFilter.getAssginmentNames()))
+                    .and(ProjectSpecs.withSummaryStatuses(projectFilter.getSummaryStatusNames()))
+                    .and(ProjectSpecs.withPrivacies(projectFilter.getPrivaciesNames())))
                 .and(ProjectSpecs.withImitsMiPlans(projectFilter.getImitsMiPlans()))
-                .and(ProjectSpecs.withProductionColonyNames(projectFilter.getProductionColonyNames()))
-                .and(ProjectSpecs.withPhenotypingExternalRefNames(projectFilter.getPhenotypingExternalRefs()));
+                .and(ProjectSpecs
+                    .withProductionColonyNames(projectFilter.getProductionColonyNames()))
+                .and(ProjectSpecs
+                    .withPhenotypingExternalRefNames(projectFilter.getPhenotypingExternalRefs())
+                    .and(ProjectSpecs.withoutNullGenesSymbols()));
+
         return specifications;
     }
 
     @Override
-    public Project createProject(Project project)
-    {
+    public Project createProject(Project project) {
         return projectCreator.createProject(project);
     }
 
     @Override
-    public History updateProject(Project oldProject, Project newProject)
-    {
+    public History updateProject(Project oldProject, Project newProject) {
         projectValidator.validatePrivacyData(oldProject, newProject);
         return projectUpdater.updateProject(oldProject, newProject);
     }
 
     @Override
-    public List<History> getProjectHistory(Project project)
-    {
+    public List<History> getProjectHistory(Project project) {
         return historyService.getHistoryByEntityNameAndEntityId("Project", project.getId());
     }
 
     @Override
-    public void checkForUpdates(Project project)
-    {
+    public void checkForUpdates(Project project) {
         projectUpdater.updateProjectWhenPlanUpdated(project);
     }
 
     @Override
-    public void associatePlanToProject(Plan plan, Project project)
-    {
-        if (project == null)
-        {
+    public void associatePlanToProject(Plan plan, Project project) {
+        if (project == null) {
             throw new UserOperationFailedException(
                 "The plan cannot be associated with the project because the project is null");
         }
-        if (plan == null)
-        {
+        if (plan == null) {
             throw new UserOperationFailedException(
                 "The plan cannot be associated with the project because the plan is null");
         }
@@ -215,13 +201,11 @@ public class ProjectServiceImpl implements ProjectService
     }
 
     @Override
-    public List<Outcome> getProductionOutcomesByProject(Project project)
-    {
+    public List<Outcome> getProductionOutcomesByProject(Project project) {
         List<Outcome> productionOutcomes = new ArrayList<>();
         List<Plan> productionPlans =
             projectQueryHelper.getPlansByType(project, PlanTypeName.PRODUCTION);
-        if (productionPlans != null)
-        {
+        if (productionPlans != null) {
             productionPlans.forEach(x -> {
                 Set<Outcome> outcomes = x.getOutcomes();
                 productionOutcomes.addAll(outcomes);
@@ -231,8 +215,8 @@ public class ProjectServiceImpl implements ProjectService
     }
 
     @Override
-    public Plan getFirstProductionPlan(Project project)
-    {
-        return project.getPlans().stream().sorted(Comparator.comparing(Plan::getId)).findFirst().get();
+    public Plan getFirstProductionPlan(Project project) {
+        return project.getPlans().stream().sorted(Comparator.comparing(Plan::getId)).findFirst()
+            .get();
     }
 }
