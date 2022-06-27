b233f42b8b5cd9a30d5a18800e6a0b1b006a2a62
ogunes <ogunes@ebi.ac.uk>
2022-06-27 14:22:38 +0100

    delete mutation button for newly added mutation added

--- impc_prod_tracker/core/src/main/java/org/gentar/biology/colony/engine/processors/ConfirmGenotypeProcessor.java
+++ impc_prod_tracker/core/src/main/java/org/gentar/biology/colony/engine/processors/ConfirmGenotypeProcessor.java
@@ -1,10 +1,12 @@
 package org.gentar.biology.colony.engine.processors;
 
+import java.util.Set;
 import org.gentar.biology.colony.Colony;
 import org.gentar.biology.colony.engine.ColonyStateSetter;
 import org.gentar.biology.mutation.Mutation;
 import org.gentar.biology.mutation.sequence.MutationSequence;
-import org.gentar.biology.plan.Plan;
+import org.gentar.biology.outcome.Outcome;
+import org.gentar.biology.plan.attempt.AttemptTypesName;
 import org.gentar.biology.sequence.SequenceService;
 import org.gentar.biology.sequence.category.SequenceCategoryName;
 import org.gentar.statemachine.AbstractProcessor;
@@ -13,71 +15,78 @@ import org.gentar.statemachine.ProcessEvent;
 import org.gentar.statemachine.TransitionEvaluation;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
-import java.util.Set;
 
 @Component
-public class ConfirmGenotypeProcessor extends AbstractProcessor
-{
+public class ConfirmGenotypeProcessor extends AbstractProcessor {
     private SequenceService sequenceService;
 
     public ConfirmGenotypeProcessor(
-        ColonyStateSetter colonyStateSetter, SequenceService sequenceService)
-    {
+        ColonyStateSetter colonyStateSetter, SequenceService sequenceService) {
         super(colonyStateSetter);
         this.sequenceService = sequenceService;
     }
 
     @Override
-    public TransitionEvaluation evaluateTransition(ProcessEvent transition, ProcessData data)
-    {
+    public TransitionEvaluation evaluateTransition(ProcessEvent transition, ProcessData data) {
         TransitionEvaluation transitionEvaluation = new TransitionEvaluation();
         transitionEvaluation.setTransition(transition);
-        boolean canExecuteTransition = canExecuteTransition((Colony)data);
-        transitionEvaluation.setExecutable(canExecuteTransition);
-        if (!canExecuteTransition)
-        {
-            transitionEvaluation.setNote("A sequence and an allele symbol must exist.");
+
+        if (((Colony) data).getOutcome().getMutations().size() == 0) {
+            transitionEvaluation
+                .setNote("The colony must have at least one mutation to move to the Genotype Confirmed state.");
+        } else {
+
+            boolean canExecuteTransition = canExecuteTransition((Colony) data);
+            transitionEvaluation.setExecutable(canExecuteTransition);
+            if (!canExecuteTransition) {
+                if (isEsCellAttempt(((Colony) data).getOutcome())) {
+                    transitionEvaluation.setNote("All mutation symbols must exist.");
+                } else {
+                    transitionEvaluation.setNote("At least one sequence and All mutation symbols must exist.");
+                }
+            }
         }
         return transitionEvaluation;
     }
 
-    private boolean canExecuteTransition(Colony colony)
-    {
-        return informationValidated(colony);
+    private boolean canExecuteTransition(Colony colony) {
+        return legacyInformationValidated(colony) &&
+            attemptTypeInformationValidated(colony.getOutcome());
     }
 
-    private boolean informationValidated(Colony colony)
-    {
-        boolean informationIsValidated = false;
-        Set<Mutation> mutations = colony.getOutcome().getMutations();
-        if (mutations != null)
-        {
-             for (Mutation mutation : mutations)
-             {
-                 Boolean legacy = colony.getLegacyWithoutSequence();
-                 if (legacy == true && legacy != null) {
-                     informationIsValidated = alleleSymbolExists(mutation);
-                     if (informationIsValidated) {
-                         break;
-                     }
-                 } else {
-                     informationIsValidated = atLeastOneOutcomeSequenceExist(mutation)
-                             && alleleSymbolExists(mutation);
-                     if (informationIsValidated) {
-                         break;
-                     }
-                 }
-             }
+    private boolean legacyInformationValidated(Colony colony) {
+        for (Mutation mutation : colony.getOutcome().getMutations()) {
+            if (colony.getLegacyWithoutSequence() && !alleleSymbolExists(mutation)) {
+                return false;
+            }
         }
-        return informationIsValidated;
+        return true;
     }
 
-    private boolean atLeastOneOutcomeSequenceExist(Mutation mutation)
-    {
+    private boolean attemptTypeInformationValidated(Outcome outcome) {
+        Set<Mutation> mutations = outcome.getMutations();
+        for (Mutation mutation : mutations) {
+            if (isEsCellAttempt(outcome)) {
+                if (!alleleSymbolExists(mutation)) {
+                    return false;
+                }
+            } else if (!atLeastOneOutcomeSequenceExist(mutation)
+                || !alleleSymbolExists(mutation)) {
+
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private String getAttemptType(Outcome outcome) {
+        return outcome.getPlan().getAttemptType().getName();
+    }
+
+    private boolean atLeastOneOutcomeSequenceExist(Mutation mutation) {
         boolean atLeastOneOutcomeSequenceExist = false;
         Set<MutationSequence> mutationSequences = mutation.getMutationSequences();
-        if (!CollectionUtils.isEmpty(mutationSequences))
-        {
+        if (!CollectionUtils.isEmpty(mutationSequences)) {
             atLeastOneOutcomeSequenceExist = mutationSequences.stream()
                 .anyMatch(x -> sequenceService.sequenceHasCategory(
                     x.getSequence(), SequenceCategoryName.OUTCOME_SEQUENCE));
@@ -85,8 +94,13 @@ public class ConfirmGenotypeProcessor extends AbstractProcessor
         return atLeastOneOutcomeSequenceExist;
     }
 
-    private boolean alleleSymbolExists(Mutation mutation)
-    {
-        return mutation.getSymbol() != null;
+    private boolean alleleSymbolExists(Mutation mutation) {
+        return mutation.getSymbol() != null && !mutation.getSymbol().equals("");
+    }
+
+    private boolean isEsCellAttempt(Outcome outcome) {
+        return getAttemptType(outcome).equals(AttemptTypesName.ES_CELL.getLabel()) ||
+            getAttemptType(outcome)
+                .equals(AttemptTypesName.ES_CELL_ALLELE_MODIFICATION.getLabel());
     }
 }
