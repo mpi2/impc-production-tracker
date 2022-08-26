4fa25a5f159ae09bdbe87914b87eb7ae733b2f7c
ogunes <ogunes@ebi.ac.uk>
2022-08-26 19:58:11 +0100

    Sequence formatter added

--- impc_prod_tracker/rest-api/src/main/java/org/gentar/biology/mutation/formatter/MutationFormatterController.java
+++ impc_prod_tracker/rest-api/src/main/java/org/gentar/biology/mutation/formatter/MutationFormatterController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/tracking-api")
+@RequestMapping("/api")
 public class MutationFormatterController {
     private final MutationFormatterServiceImpl mutationFormatterService;
 
