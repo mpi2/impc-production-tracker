0c4ff852f36cb7c23ce9076e913dcb92f5ea3e56
Robert Wilson <rwilson@ebi.ac.uk>
2022-09-22 19:11:09 +0100

    Added the GenTaR MIN identifier to the MGI Modification Report.

--- impc_prod_tracker/core/src/main/java/org/gentar/report/collection/mgi_modification_allele/MgiModificationAlleleServiceImpl.java
+++ impc_prod_tracker/core/src/main/java/org/gentar/report/collection/mgi_modification_allele/MgiModificationAlleleServiceImpl.java
@@ -68,6 +68,10 @@ public class MgiModificationAlleleServiceImpl implements MgiModificationAlleleSe
         if (productionMutationProjection != null){
             productionMutationSymbol = productionMutationProjection.getSymbol() == null ? "" : productionMutationProjection.getSymbol();
         }
+        String productionMutationMin = "";
+        if (productionMutationProjection != null){
+            productionMutationMin = productionMutationProjection.getMutationIdentificationNumber() == null ? "" : productionMutationProjection.getMutationIdentificationNumber();
+        }
 
         MgiModificationAlleleReportOutcomeMutationProjection modificationMutationProjection =
                 filteredOutcomeMutationMap.get(x.getModificationOutcomeId());
@@ -109,7 +113,8 @@ public class MgiModificationAlleleServiceImpl implements MgiModificationAlleleSe
                 x.getModificationStrainName() + "\t" +
                 x.getModificationWorkUnit() + "\t" +
                 modificationMgiAlleleAccId + "\t" +
-                modificationFormattedMutationSymbol;
+                modificationFormattedMutationSymbol + "\t" +
+                productionMutationMin;
     }
 
     private String checkMutationSymbol(String mutationSymbol, String geneSymbol) {
@@ -160,7 +165,8 @@ public class MgiModificationAlleleServiceImpl implements MgiModificationAlleleSe
                 "Modification Plan Colony Background Strain",
                 "Modification Work Unit",
                 "Modification Mutation Accession ID",
-                "Modification Mutation Symbol"
+                "Modification Mutation Symbol",
+                "GenTaR Mutation Identifier"
         );
 
         String headerString =   headers
