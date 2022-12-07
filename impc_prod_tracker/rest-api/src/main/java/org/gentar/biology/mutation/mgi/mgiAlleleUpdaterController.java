90e304b89db8cd331260bf2334a225182b2d0d9c
ogunes <ogunes@ebi.ac.uk>
2022-12-07 12:40:42 +0000

    mutation mgiallele Id updater endpoint added

--- impc_prod_tracker/rest-api/src/main/java/org/gentar/biology/mutation/mgi/mgiAlleleUpdaterController.java
+++ impc_prod_tracker/rest-api/src/main/java/org/gentar/biology/mutation/mgi/mgiAlleleUpdaterController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/tracking-api")
+@RequestMapping("/api")
 public class mgiAlleleUpdaterController {
     private final MutationServiceImpl mutationService;
 
@@ -19,7 +19,6 @@ public class mgiAlleleUpdaterController {
         this.mutationService = mutationService;
     }
 
-    // http://localhost:8080/tracking-api/mgiAllele
     @PutMapping(value = {"/mutation/mgiAllele/update"})
     public MgiAlleleResponseDto update(HttpServletResponse response, @RequestBody List<MgiAlleleDto> mgiAlleleDto) {
         return mutationService.updateMutationMgiAlleleId(mgiAlleleDto);
