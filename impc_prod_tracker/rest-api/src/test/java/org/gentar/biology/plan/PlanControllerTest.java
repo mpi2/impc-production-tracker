f15e53e6d97314c675aa69e7bed8747e8ad9e0d9
ogunes <ogunes@ebi.ac.uk>
2023-12-11 11:29:44 +0000

    GID added

--- impc_prod_tracker/rest-api/src/test/java/org/gentar/biology/plan/PlanControllerTest.java
+++ impc_prod_tracker/rest-api/src/test/java/org/gentar/biology/plan/PlanControllerTest.java
@@ -183,7 +183,7 @@ class PlanControllerTest extends ControllerTestTemplate
         assertThat(historyDTO.getComment(), is("Plan updated"));
 
         List<HistoryDetailDTO> historyDetailDTOS = historyDTO.getDetails();
-        assertThat(historyDetailDTOS.size(), is(4));
+        assertThat(historyDetailDTOS.size(), is(6));
 
         HistoryDetailDTO historyDetailDTO1 =
                 getHistoryDetailByField(historyDetailDTOS, "comment");
