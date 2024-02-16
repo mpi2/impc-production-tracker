ca403e7681743cdcfa744343e0bbf365749b556e
ogunes <ogunes@ebi.ac.uk>
2024-02-16 12:01:32 +0000

    distribution network all names endpoint added

--- impc_prod_tracker/core/src/main/java/org/gentar/biology/colony/distribution/distribution_network/DistributionNetworkRepository.java
+++ impc_prod_tracker/core/src/main/java/org/gentar/biology/colony/distribution/distribution_network/DistributionNetworkRepository.java
@@ -2,7 +2,12 @@ package org.gentar.biology.colony.distribution.distribution_network;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface DistributionNetworkRepository extends CrudRepository<DistributionNetwork, Long>
 {
     DistributionNetwork findByNameIgnoreCase(String name);
+
+    List<DistributionNetwork> findAll();
+
 }
--- impc_prod_tracker/core/src/main/java/org/gentar/biology/colony/distribution/distribution_network/DistributionNetworkService.java
+++ impc_prod_tracker/core/src/main/java/org/gentar/biology/colony/distribution/distribution_network/DistributionNetworkService.java
@@ -3,28 +3,30 @@ package org.gentar.biology.colony.distribution.distribution_network;
 import org.gentar.exceptions.UserOperationFailedException;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
-public class DistributionNetworkService
-{
+public class DistributionNetworkService {
     private DistributionNetworkRepository distributionNetworkRepository;
 
-    public DistributionNetworkService(DistributionNetworkRepository distributionNetworkRepository)
-    {
+    public DistributionNetworkService(DistributionNetworkRepository distributionNetworkRepository) {
         this.distributionNetworkRepository = distributionNetworkRepository;
     }
 
-    public DistributionNetwork getDistributionNetworkByName(String name)
-    {
+    public DistributionNetwork getDistributionNetworkByName(String name) {
         return distributionNetworkRepository.findByNameIgnoreCase(name);
     }
 
-    public DistributionNetwork getDistributionNetworkByNameFailIfNull(String name)
-    {
+    public DistributionNetwork getDistributionNetworkByNameFailIfNull(String name) {
         DistributionNetwork distributionNetwork = getDistributionNetworkByName(name);
-        if (distributionNetwork == null)
-        {
+        if (distributionNetwork == null) {
             throw new UserOperationFailedException("Distribution Network " + name + " does not exist");
         }
         return distributionNetwork;
+    }
+
+    public List<DistributionNetwork> getAllDistributionNetworks() {
+        return distributionNetworkRepository.findAll();
+
     }
 }
