283642998ded1da532f4bd83de7fe04496e4a2b8
ogunes-ebi <ogunes@ebi.ac.uk>
2025-07-29 14:02:18 +0100

    canonical and targeted exons and added for insertion sequences

--- impc_prod_tracker/rest-api/src/main/java/org/gentar/biology/mutation/InsertionSequenceMapper.java
+++ impc_prod_tracker/rest-api/src/main/java/org/gentar/biology/mutation/InsertionSequenceMapper.java
@@ -2,7 +2,13 @@ package org.gentar.biology.mutation;
 
 import org.gentar.Mapper;
 import org.gentar.biology.insertion_sequence.InsertionSequence;
+import org.gentar.biology.plan.attempt.crispr.canonical_targeted_exon.InsertionCanonicalTargetedExon;
+import org.gentar.biology.plan.attempt.crispr.targeted_exon.InsertionTargetedExon;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 public class InsertionSequenceMapper implements Mapper<InsertionSequence, InsertionSequenceDTO> {
@@ -36,6 +42,8 @@ public class InsertionSequenceMapper implements Mapper<InsertionSequence, Insert
             insertionSequence.setSequence(insertionSequenceDTO.getSequence());
             insertionSequence.setLocation(insertionSequenceDTO.getLocation());
             insertionSequence.setIns(buildIns(insertionSequenceDTO.getId()));
+            setInsertionCanonicalTargetedExons(insertionSequence,insertionSequenceDTO);
+            setInsertionTargetedExons(insertionSequence,insertionSequenceDTO);
         }
 
         return insertionSequence;
@@ -46,4 +54,35 @@ public class InsertionSequenceMapper implements Mapper<InsertionSequence, Insert
         identifier = "INS:" + identifier;
         return identifier;
     }
+
+    private void setInsertionCanonicalTargetedExons(InsertionSequence insertionSequence ,InsertionSequenceDTO insertionSequenceDTO)
+    {
+        List<InsertionCanonicalTargetedExonDTO> dtoList = insertionSequenceDTO.getInsertionCanonicalTargetedExonsDTO();
+
+        Set<InsertionCanonicalTargetedExon> exonSet = dtoList.stream()
+                .map(dto -> {
+                    InsertionCanonicalTargetedExon exon = insertionCanonicalTargetedExonMapper.toEntity(dto);
+                    exon.setInsertionSequence(insertionSequence); // establish parent-child relationship
+                    return exon;
+                })
+                .collect(Collectors.toSet());
+
+        insertionSequence.setInsertionCanonicalTargetedExons(exonSet);
+    }
+
+    private void setInsertionTargetedExons(InsertionSequence insertionSequence ,InsertionSequenceDTO insertionSequenceDTO)
+    {
+        List<InsertionTargetedExonDTO> dtoList = insertionSequenceDTO.getInsertionTargetedExonsDTO();
+
+        Set<InsertionTargetedExon> exonSet = dtoList.stream()
+                .map(dto -> {
+                    InsertionTargetedExon exon = insertionTargetedExonMapper.toEntity(dto);
+                    exon.setInsertionSequence(insertionSequence); // establish parent-child relationship
+                    return exon;
+                })
+                .collect(Collectors.toSet());
+
+        insertionSequence.setInsertionTargetedExons(exonSet);
+    }
+
 }
